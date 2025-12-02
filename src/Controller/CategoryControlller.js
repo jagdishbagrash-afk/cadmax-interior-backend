edb45fbb895ab6a10efdbbedecd08d9a56f8a6f9
ankitjain0748 <ankitjain0748@gmail.com>
2025-12-02 21:10:25 +0530

    Image and register Done

--- src/Controller/CategoryControlller.js
+++ src/Controller/CategoryControlller.js
@@ -1,4 +1,4 @@
-const Category = require("../Model/Category");
+const Category = require("../Model/Categroy");
 const CatchAsync = require("../Utill/catchAsync");
 const { errorResponse, successResponse, validationErrorResponse } = require("../Utill/ErrorHandling");
 const { deleteFile } = require("../Utill/S3");
