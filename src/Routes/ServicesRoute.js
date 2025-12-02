db7e7d89f10eb1eef8a02204eb071d1aca4834e5
ankitjain0748 <ankitjain0748@gmail.com>
2025-12-02 21:23:44 +0530

    Image and register Done 3

--- src/Routes/ServicesRoute.js
+++ src/Routes/ServicesRoute.js
@@ -8,5 +8,4 @@ router.get("/services/list", servicesController.getAllServices);
 router.get("/services/list/:id", servicesController.getServiceById);  
 router.post("/services/edit/:id", servicesController.updateService);   
 router.post("/services/delete/:id", servicesController.deleteService);
-
 module.exports = router;
