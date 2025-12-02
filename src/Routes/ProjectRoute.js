7651bae996ca8ea2fb456214c71c74b9ca7ac3ca
ankitjain0748 <ankitjain0748@gmail.com>
2025-12-02 22:42:53 +0530

    Image and register Done 3

--- src/Routes/ProjectRoute.js
+++ src/Routes/ProjectRoute.js
@@ -5,7 +5,7 @@ const router = require("express").Router();
 
 router.post("/project/add", upload.single("image"), AddProject);
 router.get("/project/list", GetAllProject);
-router.get("/project/:id", GetProjectById);
+router.get("/project/details/:id", GetProjectById);
 router.post("/project/edit/:id", upload.single("image"), UpdateProject);
 router.post("/project/status/:id", ToggleProjectStatus);
 router.post("/project/status", GetAllProjectStatus);
