79ac52e4b6eb7e51205b61e81d8242fba3de7a0b
Abhinav Mathur <mathur.abhinav1108@gmail.com>
2025-12-02 21:31:59 +0530

    JWT updated

--- src/Utill/tokenVerify.js
+++ src/Utill/tokenVerify.js
@@ -9,10 +9,11 @@ exports.verifyToken = (req, res, next) => {
         message: 'Token missing or invalid'
       });
     }
+    console.log("authHeader", authHeader);
 
     const token = authHeader.split(' ')[1];
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
