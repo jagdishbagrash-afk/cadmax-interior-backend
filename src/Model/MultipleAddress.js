143d542c167141a6dd0f87f143bd9974b92e2069
ankitjain <131745126+ankitjain0748@users.noreply.github.com>
2026-03-14 17:32:16 +0530

    Add MultipleAddress model with address schema

---
+++ src/Model/MultipleAddress.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  pincode: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  addressLine1: {
+    type: String,
+  },
+  addressLine2: {
+    type: String,
+  },
+  isDefault: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("Address", addressSchema);
