c9377cf143585f96ecc10ec9005cf48eef41956c
pradeep-1316 <pardeepsaini2206@gmail.comn>
2026-06-12 15:57:01 +0530

    add review and rating

--- src/Routes/AppRoute.js
+++ src/Routes/AppRoute.js
@@ -80,7 +80,7 @@ AppRoute.delete("/app/wishlist/delete/:productId", verifyToken, removeFromWishli
 
 
 
-// ========== APP REVIEW ROUTES ==========
+// ========== APP RATING AND REVIEW ROUTES ==========
 // Public
 AppRoute.get("/app/review/product/:productId", getProductReviews);
 AppRoute.get("/app/review/rating-summary/:productId", getProductRatingSummary);
