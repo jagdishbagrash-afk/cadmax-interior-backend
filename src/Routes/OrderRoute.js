99098ff28e286fc46c9ad10968bf6fa6675f610f
sainipinku <ramchandrasaini63@gmail.com>
2026-08-06 17:28:14 +0530

    udpate order rute

--- src/Routes/OrderRoute.js
+++ src/Routes/OrderRoute.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const { addOrder, getAllOrders, getOrdersByUser, updateStatus, getOrderDetailsWeb, getOrderDetailsApp, getOrderById } = require("../Controller/OrderController");
+const { addOrder, getAllOrders, getOrdersByUser, updateStatus, getOrderDetailsWeb, getOrderDetailsApp, getOrderById, getAllOrdersAdmin } = require("../Controller/OrderController");
 const { verifyToken } = require("../Utill/tokenVerify");
 
 router.post("/order/add", verifyToken, addOrder);
 router.get("/order/getAll", getAllOrders);
 router.post("/order/status/update/:id", verifyToken, updateStatus);
-router.get("/order/getbyUser", verifyToken, getOrdersByUser);   
+router.get("/order/getbyUser", verifyToken, getOrdersByUser);
 
 router.get("/order/:id", getOrderById);
 
